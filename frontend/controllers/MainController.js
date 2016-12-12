153b447e97d0ca6ba8655293a8e7cb2ed5b6c22a
Michael Tjandra <miciltjandra@gmail.com>
2016-12-12 10:49:50 +0700

    Reports from json

--- frontend/controllers/MainController.js
+++ frontend/controllers/MainController.js
@@ -1,8 +1,22 @@
-MainController.$inject = ['$scope'];
+MainController.$inject = ['$scope', '$location', '$http'];
 
-function MainController($scope) {
+function MainController($scope, $location, $http) {
   //$scope.name = 'world';
   $scope.names = ['a', 'b', 'c'];
+
+  $scope.getClass = function (path) {
+    return ($location.path().substr(0, path.length) === path) ? 'active' : '';
+  };
+
+  $http.get("report.json").then(function(response) {
+    $scope.reports = response.data;
+    console.log($scope.reports);
+  });
+
+  $http.get("user.json").then(function(response) {
+    $scope.users = response.data;
+    console.log($scope.users);
+  });  
 }
 
 export default MainController;
